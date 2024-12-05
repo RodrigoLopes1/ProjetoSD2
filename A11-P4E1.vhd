74b78355460c63221be6cc91d228cf66ecc470ad
RodrigoLopes1 <105373936+RodrigoLopes1@users.noreply.github.com>
2024-12-05 19:49:32 -0300

    Create A11-P4E1.vhd

---
+++ A11-P4E1.vhd
@@ -0,0 +1,12 @@
+--Essa oficialmente é a parte principal, todo resto além disso é opcional, mas tentar implementar tudo de uma vez é bem pior (btw da pra entregar as outras partes então pode ser bom
+--entregar pra sla chorar uns pontinhos) 
+--
+--Basicamente o objetivo dessa parte é juntar todo que está feito e rodar em uma testbench, na teoria é simples (e de fato é) mas tem que fazer de fato..rs
+
+
+
+
+
+--A parte 1 da entrega é só fazer um protótipo do fluxo de dados do processador, interligando o banco de registradores e a ULA para executar as instruções do tipo R
+--Nessa parte já seria necessario fazer uma testbench pra ver se está tudo ok, e por mais que não tenha que entregar a testbench, tem que fazer de qualquer forma porque tem que entregar 
+--Os graficos no gtkwave. 

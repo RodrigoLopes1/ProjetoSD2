71c562cf84b50e4eb5e0b9812aa85e269f3c31f2
RodrigoLopes1 <105373936+RodrigoLopes1@users.noreply.github.com>
2024-12-07 19:36:37 -0300

    Update AF11-P3E2.vhd

--- Parte3-ULA/Designs/AF11-P3E2.vhd
+++ Parte3-ULA/Designs/AF11-P3E2.vhd
@@ -1,89 +1,71 @@
---Esse também tá incompleto, na v24 é pedido que sejá feito uma Ula de 64bits (baseado no que foi feito no P3E1, isso é igual) 
---mas eles querem uma Ula com especificamente 64bits enquanto antes era pedido uma ULA com parametros genéricos (o que teoricamente é mais dificil) 
---Vou deixar aqui a com 64 bits para seguir de exemplo
-
 library ieee;
 use ieee.numeric_bit.all;
 
 entity alu is
-    generic (
-        size : natural := 10
+    generic(
+        size: natural := 64 -- Especificamente 64 bits
     );
-    port (
-        A, B : in bit_vector(size - 1 downto 0);
-        F : out bit_vector(size - 1 downto 0);
-        S : in bit_vector(3 downto 0);
-        Z : out bit;
-        Ov : out bit;
-        Co : out bit
+    port(
+        A, B: in bit_vector(size-1 downto 0);
+        F: out bit_vector(size-1 downto 0);
+        S: in bit_vector(3 downto 0);
+        Z, Ov, Co: out bit
     );
-end entity alu;
+end entity;
 
-architecture ALU_arch of alu is
+architecture stuctural of alu is
+
     component alu1bit is
-        port (
-            a, b, less, cin : in bit;
-            result, cout, set, overflow : out bit;
-            ainvert, binvert : in bit;
-            operation : in bit_vector(1 downto 0)
+        port(
+            a, b, less, cin: in bit;
+            result, cout, set, overflow: out bit;
+            ainvert, binvert: in bit;
+            operation: in bit_vector(1 downto 0)
         );
     end component;
 
-    signal less_interno : bit;
-    signal v_saidas, v_sets, v_carries : bit_vector(size - 1 downto 0);
+    signal ainvert, binvert: bit;
+    signal operation: bit_vector(1 downto 0);
+    signal coutBv: bit_vector(size-2 downto 0);
+    signal resultBv, setBv, overflowBv, zero: bit_vector(size-1 downto 0);
 
 begin
-    -- ULA de 1 bit mais significativa
-    alu_ms : alu1bit port map(
-        A(size - 1), 
-        B(size - 1), 
-        '0', 
-        v_carries(size - 2), 
-        v_saidas(size - 1),
-        v_carries(size - 1), 
-        v_sets(size - 1), 
-        Ov, 
-        S(3), 
-        S(2), 
-        S(1 downto 0)
-    );
 
-    -- ULAs do meio
-    ULA_GERADOR : for i in 1 to (size - 2) generate
-        alu_x : alu1bit port map(
-            A(i), 
-            B(i), 
-            '0', 
-            v_carries(i - 1), 
-            v_saidas(i), 
-            v_carries(i), 
-            v_sets(i), 
-            open, 
-            S(3), 
-            S(2), 
-            S(1 downto 0)
+    ainvert <= S(3);
+    binvert <= S(2);
+    operation <= S(1 downto 0);
+
+    -- Primeira ALU (menos significativa)
+    firstAlu: alu1bit port map(
+        A(0), B(0), '0', binvert,
+        resultBv(0), coutBv(0), setBv(0), overflowBv(0),
+        ainvert, binvert,
+        operation
+    );
+    zero(0) <= resultBv(0);
+
+    -- Última ALU (mais significativa)
+    lastAlu: alu1bit port map(
+        A(size-1), B(size-1), '0', coutBv(size-2),
+        resultBv(size-1), Co, setBv(size-1), Ov,
+        ainvert, binvert,
+        operation
+    );
+    zero(size-1) <= zero(size-2) or resultBv(size-1);
+
+    -- ALUs intermediárias
+    Alus: for i in size-2 downto 1 generate
+        Alu_i: alu1bit port map(
+            A(i), B(i), '0', coutBv(i-1),
+            resultBv(i), coutBv(i), setBv(i), overflowBv(i),
+            ainvert, binvert,
+            operation
         );
+        zero(i) <= zero(i-1) or resultBv(i);
     end generate;
 
-    -- ULA de 1 bit menos significativa
-    alu_ls : alu1bit port map(
-        A(0), 
-        B(0), 
-        less_interno, 
-        S(2), 
-        v_saidas(0), 
-        v_carries(0), 
-        v_sets(0), 
-        open,
-        S(3), 
-        S(2), 
-        S(1 downto 0)
-    );
+    -- Definição das saídas
+    Z <= not zero(size-1); -- Flag de zero
+    F <= resultBv;         -- Resultado final
 
-    -- Definição de sinais
-    less_interno <= v_sets(size - 1);
-    Co <= v_carries(size - 1);
-    Z <= '1' when (signed(v_saidas) = 0) else '0';
-    F <= v_saidas;
-
-end architecture ALU_arch;
+end architecture;
